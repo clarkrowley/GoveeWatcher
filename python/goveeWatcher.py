7fcbb09f2def789fd62ce347181475ed2bcac08a
Clark Rowley <clarkdavidrowley@gmail.com>
2023-07-11 21:06:43 -0500

    Clean up simple python script for my Govee use.

--- python/goveeWatcher.py
+++ python/goveeWatcher.py
@@ -3,112 +3,94 @@
 from time import sleep
 import os
 import sys
+import struct
+from datetime import datetime
 
-from bleson import get_provider, Observer, UUID16
+from bleson import get_provider, Observer #, UUID16
 from bleson.logger import log, set_level, ERROR, DEBUG
 
-# Disable warnings
-set_level(ERROR)
+class GoveeWatcher:
 
-# # Uncomment for debug log level
-# set_level(DEBUG)
+  def __init__(self):
+    self.govee_devices = {}
+    self.start()
 
-# https://macaddresschanger.com/bluetooth-mac-lookup/A4%3AC1%3A38
-# OUI Prefix	Company
-# A4:C1:38	Telink Semiconductor (Taipei) Co. Ltd.
-GOVEE_BT_mac_OUI_PREFIX = "A4:C1:38"
+    # Disable warnings
+    set_level(ERROR)
+    # # Uncomment for debug log level
+    # set_level(DEBUG)
 
-H5075_UPDATE_UUID16 = UUID16(0xEC88)
-
-govee_devices = {}
-
-# ###########################################################################
-FORMAT_PRECISION = ".2f"
-
-
-# Decode H5075 Temperature into degrees Celcius
-def decode_temp_in_c(encoded_data):
-    return format((encoded_data / 10000), FORMAT_PRECISION)
-
-
-# Decode H5075 Temperature into degrees Fahrenheit
-def decode_temp_in_f(encoded_data):
-    return format((((encoded_data / 10000) * 1.8) + 32), FORMAT_PRECISION)
-
-
-# Decode H5075 percent humidity
-def decode_humidity(encoded_data):
-    return format(((encoded_data % 1000) / 10), FORMAT_PRECISION)
-
-
-def print_values(mac):
-    govee_device = govee_devices[mac]
-    print(
-        f"{govee_device['name']} ({govee_device['address']}) - \
-Temperature {govee_device['tempInC']}C / {govee_device['tempInF']}F  - \
-Humidity: {govee_device['humidity']}% - \
-Battery:  {govee_device['battery']}%"
+  def print_values(self,mac):
+    govee_device = self.govee_devices[mac]
+    fmt='%4s %6s - T: %4.1f C / %5.1f F - RH: %5.1f %% - Bat: %3.0f %%'
+    print( fmt % (
+       govee_device['name'],
+       govee_device['now'],
+       govee_device['tempInC'],
+       govee_device['tempInF'],
+       govee_device['humidity'],
+       govee_device['battery'] )
     )
 
-
-def print_rssi(mac):
+  def print_rssi(self,mac):
     govee_device = govee_devices[mac]
     print(
-        f"{govee_device['name']} ({govee_device['address']}) - \
-RSSI: {govee_device['rssi']}"
+        f"{govee_device['name']} - RSSI: {govee_device['rssi']}"
     )
 
+  def decode_temp_humid_bat(self,advertisement) -> tuple[float, float, float]:
+    temp, hum, bat = struct.unpack_from("<HHB", advertisement.mfg_data[-5:])
+    if temp & (1 << 15):
+      temp = temp - (1 << 16)
+    if hum & (1 << 15):
+      hum = hum - (1 << 16)
+    temp /= 100
+    hum /= 100
+    return temp, hum, bat
 
-# On BLE advertisement callback
-def on_advertisement(advertisement):
-    log.debug(advertisement)
+  # On BLE advertisement callback
+  def on_advertisement(self,advertisement):
 
-    if advertisement.address.address.startswith(GOVEE_BT_mac_OUI_PREFIX):
-        mac = advertisement.address.address
+    if advertisement.name is not None:
+      if advertisement.name.startswith('Govee_H5179'):
+        now = datetime.now().strftime("%H:%M:%S")
+        self.mac = advertisement.name.split("_")[2]
+        if self.mac not in self.govee_devices:
+          self.govee_devices[self.mac] = {}
+        self.govee_devices[self.mac]["name"] = self.mac
+        self.govee_devices[self.mac]["now"] = now
 
-        if mac not in govee_devices:
-            govee_devices[mac] = {}
-        if H5075_UPDATE_UUID16 in advertisement.uuid16s:
-            # HACK:  Proper decoding is done in bleson > 0.10
-            name = advertisement.name.split("'")[1]
+    if advertisement.mfg_data is not None:
+      encoded_data = int(advertisement.mfg_data.hex()[0:5], 16)
+      if encoded_data == 6286:
+        tempC,rh,bat = self.decode_temp_humid_bat(advertisement)
+        tempF = tempC * 1.8 + 32
+        self.govee_devices[self.mac]["tempInC"] = tempC
+        self.govee_devices[self.mac]["tempInF"] = tempF
+        self.govee_devices[self.mac]["humidity"] = rh
+        self.govee_devices[self.mac]["battery"] = bat
+        self.print_values(self.mac)
 
-            encoded_data = int(advertisement.mfg_data.hex()[6:12], 16)
-            battery = int(advertisement.mfg_data.hex()[12:14], 16)
-            govee_devices[mac]["address"] = mac
-            govee_devices[mac]["name"] = name
-            govee_devices[mac]["mfg_data"] = advertisement.mfg_data
-            govee_devices[mac]["data"] = encoded_data
+  def start(self):
+    self.adapter = get_provider().get_adapter()
+    print(self.adapter)
 
-            govee_devices[mac]["tempInC"] = decode_temp_in_c(encoded_data)
-            govee_devices[mac]["tempInF"] = decode_temp_in_f(encoded_data)
-            govee_devices[mac]["humidity"] = decode_humidity(encoded_data)
-
-            govee_devices[mac]["battery"] = battery
-            print_values(mac)
-        if advertisement.rssi is not None and advertisement.rssi != 0:
-            govee_devices[mac]["rssi"] = advertisement.rssi
-            print_rssi(mac)
-
-        log.debug(govee_devices[mac])
-
-
-# ###########################################################################
-
-
-adapter = get_provider().get_adapter()
-
-observer = Observer(adapter)
-observer.on_advertising_data = on_advertisement
+    self.observer = Observer(self.adapter)
+    print(self.observer)
+    self.observer.on_advertising_data = self.on_advertisement
 
 try:
-    while True:
-        observer.start()
-        sleep(2)
-        observer.stop()
+  watcher = GoveeWatcher()
+  while True:
+    watcher.observer.start()
+    sleep(2)
+    watcher.observer.stop()
+
 except KeyboardInterrupt:
-    try:
-        observer.stop()
-        sys.exit(0)
-    except SystemExit:
-        observer.stop()
-        os._exit(0)
+  try:
+    watcher.observer.stop()
+    sys.exit(0)
+  except SystemExit:
+    watcher.observer.stop()
+    os._exit(0)
+
